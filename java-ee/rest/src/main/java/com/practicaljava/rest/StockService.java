1cb2462bd29751aaf2d10c18fe780b66eabcb1a7
Mykola Golubyev <golubev.nikolay@gmail.com>
2010-11-14 20:45:40 +0800

    Added JAX-RS (REST) example.

---
+++ java-ee/rest/src/main/java/com/practicaljava/rest/StockService.java
@@ -0,0 +1,30 @@
+package com.practicaljava.rest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockService {
+    public static void addStock(Stock stock) {
+        stocks.put(stock.getSymbol(), stock);
+    }
+
+    public static void removeStock(String symbol) {
+        stocks.remove(symbol);
+    }
+
+    public static Stock getStock(String symbol) {
+        return stocks.get(symbol);
+    }
+
+    private static Map<String, Stock> stocks = new HashMap<String, Stock>();
+
+    static {
+        generateStocks();
+    }
+
+    private static void generateStocks() {
+        addStock(new Stock("IBM", 43.12, "USD", "US"));
+        addStock(new Stock("APPL", 320.0, "USD", "US"));
+     }
+
+}
